beff29f591c95b8fa7b004148af3b6ae16a01e94
arvin_e <ajescolano@gmail.com>
2023-10-28 03:16:53 +0800

    Update cleaning_utils.py

--- py_functions/cleaning_utils.py
+++ py_functions/cleaning_utils.py
@@ -136,7 +136,8 @@ def construct_projects_df(df):
     >>> construct_projects_df(df)
     # Projects DataFrame
     """
-    projects_df = df_project.value_counts().reset_index()
+    projects_df = df.project.value_counts()
+    projects_df = projects_df.reset_index()
     cols = projects_df.columns
     projects_df.columns = ['index']+cols[1:]
     projects_df['soundex'] = projects_df['index'].map(soundex)
