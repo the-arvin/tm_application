0c3a9580a669bf3486f3e5100d8b8546da861ab0
arvin_e <ajescolano@gmail.com>
2023-10-28 00:08:44 +0800

    Add files via upload

---
+++ py_functions/cleaning_utils.py
@@ -0,0 +1,284 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 25 21:55:23 2023
+
+@author: Arvin Jay
+"""
+
+import pandas as pd
+import dateparser
+from jellyfish import soundex
+from fuzzywuzzy import fuzz
+
+hard_coded_dict = {
+        'traffic':'transit',
+        'misc':'miscellaneous',
+        # 'leave':None,
+        # 'random':None,
+        # 'overthinking':None,
+        # 'fatnesscheckin':None,
+    }
+
+def generate_url(csv_link):
+    """
+    Generate a Google Drive URL from a CSV link.
+
+    Parameters:
+    csv_link (str): A link to a CSV file from TM's Google Drive.
+
+    Returns:
+    str: The Google Drive URL.
+    
+    >>> generate_url('https://drive.google.com/file/d/1HAq1aVDrMO48BTmTM5DCrZgA1iPEDG6Y/view')
+    'https://drive.google.com/uc?id=1HAq1aVDrMO48BTmTM5DCrZgA1iPEDG6Y'
+    """
+    source_id = csv_link.split('/')[-2]
+    return f'https://drive.google.com/uc?id={source_id}'
+
+def custom_parser(unparsed_timestamp):
+    """
+    Customized timestamp parser to handle various timestamp formats.
+
+    Parameters:
+    unparsed_timestamp (str): The unparsed timestamp.
+        pd.to_datetime(unparsed_timestamp) handles the more common datetime formats
+        dateparser.parse(unparsed_timestamp) handles the russian datetime formats
+        
+    Returns:
+    pd.Timestamp: The parsed timestamp.
+    
+    >>> custom_parser('2019-09-27 00:00:00 UTC')
+    '2019-09-27 00:00:00 UTC'
+    >>> custom_parser('26 сентября 2019 00:00')
+    '2019-09-26 00:00:00 UTC'
+    """
+    try:
+        return pd.to_datetime(unparsed_timestamp).tz_localize(None)
+    except ValueError:
+        try:
+            return dateparser.parse(unparsed_timestamp)
+        except ValueError:
+            return pd.NaT
+
+def load_csv(url): # add try except
+    """
+    Load data from a CSV file and parse timestamps.
+    
+    Parameters:
+    url (str): The URL of the CSV file.
+    
+    Returns:
+    pd.DataFrame: The loaded DataFrame.
+    
+    >>> load_csv(url)
+    pd.DataFrame of data from Google Drive where the column 'timestamp' has
+    been parsed using custom parser
+    """
+    return pd.read_csv(url, parse_dates=['timestamp'], date_parser=custom_parser)
+
+
+def clean_df(df,correction_dict):
+    """
+    Parameters
+    ----------
+    df : pandas Dataframe
+        the working dataframe loaded previously.
+    correction_dict : dictionary
+        a dictionary created from fuzzy matching and sound indexing.
+
+    Returns:
+    df : where 
+            null values in 'user' are dropped
+            considered number of checkin hours is >0
+            correction_dict is applied to 'project' column
+        
+    >>> clean_df(df,correction_dict)
+    pd.DataFrame of cleaned data that has been loaded. The column 'project'
+    has been corrected for fuzzymatched and phonetically matching terms
+    """
+    df = df[df['user'].notnull()]#df.fillna('Unknown')
+    df = df[df['hours']>0]
+    df['project']=df['project'].apply(lambda x: apply_correction(correction_dict,x))
+    df['timestamp'] = df['timestamp'].apply(lambda x: make_naive(x))
+    return df
+    
+
+def calculate_similarity(entry1, entry2):
+    """
+    Calculate the similarity between two strings using the fuzz.ratio method.
+
+    Parameters:
+    entry1 (str): The first string.
+    entry2 (str): The second string.
+
+    Returns:
+    int: The similarity score.
+    
+    >>> calculate_similarity('project-30', 'project-31')
+    90
+    """
+    return fuzz.ratio(entry1, entry2)
+
+
+def construct_projects_df(df):
+    """
+    Construct a DataFrame of project names and their counts, soundex values, 
+    shifted project names, along with similarity scores.
+
+    Parameters:
+    df (pd.DataFrame): The input loaded DataFrame.
+
+    Returns:
+    pd.DataFrame: The constructed projects DataFrame.
+    
+    >>> construct_projects_df(df)
+    # Projects DataFrame
+    """
+    projects_df = df.project.value_counts().reset_index()
+    projects_df['soundex'] = projects_df['index'].map(soundex)
+    projects_df = projects_df.sort_values(by=['soundex','project'])
+    
+    projects_df['shifted'] = projects_df['index'].shift(1)
+    projects_df['similarity_score'] = projects_df.apply(lambda row: calculate_similarity(row['index'], row['shifted']), axis=1)
+    return projects_df
+
+def process_group(df_temp, correction_dict):
+    """
+    Process a group of similar project names and update the correction 
+    dictionary for similar terms in the 'project' column.
+
+    Parameters:
+    df_temp (list): A list of project names with similarity.
+    correction_dict (dict): The correction dictionary for project names.
+
+    Returns:
+    dict: Updated correction dictionary.
+    
+    >>> process_group(sample_group, sample_correction_dict)
+    # Updated correction dictionary
+    """    
+    df_temp = pd.DataFrame(df_temp)
+    df_temp = df_temp.sort_values(by='project', ascending=False)
+    elements = list(set(df_temp['index'].tolist()+df_temp['shifted'].tolist()))
+    for entry in elements:
+        correction_dict[entry] = df_temp['index'].iloc[0]
+    return correction_dict
+
+def generate_correction_dict(df):
+    """
+    Parameters
+    ----------
+    df : pandas DataFrame
+        
+        The default is projects_df-  which contains columns 
+        for the index (project name), project (count),
+        shifted (shifted project name), and similarity_score (fuzzywuzzy 
+        similarity score). This function iterates over the rows of df to create 
+        a correction dictionary. The criteria for the correction is having a 
+        similar soundex score and a similarity score of greater than 90.
+
+    Returns
+    -------
+    correction_dict : dictionary
+    
+        Dictionary that would be used as a tool to clean-up project 
+        descriptions
+        
+        >>> generate_correction_dict(sample_dataframe)
+        
+                    {
+                      "blog-ideas": "blogideas",
+                      "cultureandmangement": "cultureandmanagement",
+                      "cultureandmanagemen": "cultureandmanagement",
+                      "clutureandmanagement": "cultureandmanagement",
+                      "culturandmanagement": "cultureandmanagement",
+                      "hirng": "hiring",
+                      "internal": "internals",
+                      "machine-learning": "machinelearning",
+                      "opsandamin": "opsandadmin",
+                      "opsandadmin": "opsandadmin",
+                      "opsadadmin": "opsandadmin",
+                      "opsandandmin": "opsandadmin",
+                      "opssandadmin": "opsandadmin",
+                      "opdandadmin": "opsandadmin"
+                    }
+    """
+    df_temp = list()
+    correction_dict = dict()
+    for index, row in df.iterrows():
+        if row['similarity_score'] > 90:
+            df_temp.append(row)
+        elif len(df_temp)> 0:
+            correction_dict = process_group(df_temp, correction_dict)
+            df_temp = list()
+        else:
+            pass
+    if df_temp:
+        correction_dict = process_group(df_temp, correction_dict)
+    return correction_dict
+
+def make_naive(x):
+    """
+    Convert a datetime series to a naive datetime series.
+
+    Parameters:
+    x : datetime object that may or may not be timezone aware
+
+    Returns:
+    x : naive datetime object
+
+    >>> make_naive(pd.Series(['2023-10-25 12:00:00']))
+    Timestamp('2023-10-25 12:00:00')
+    """
+    try:
+        x = pd.to_datetime(x).dt.tz_convert(None)
+    except:
+        pass
+    return x
+
+def apply_correction(correction_dict, value):
+    """
+    Apply corrections to a project name using the correction dictionary.
+
+    Parameters:
+    correction_dict (dict): The correction dictionary for project names.
+    value (str): The project name to be corrected.
+
+    Returns:
+    str: The corrected project name.
+    
+    >>> apply_correction(correction_dict, 'opdandadmin')
+    'opsandadmin'
+    """
+    try:
+        value = correction_dict[value]
+    except KeyError:
+        pass
+    return value
+
+def cleaning_process(hard_coded_link):
+    """
+    Perform the data cleaning process, including loading data, 
+    constructing a correction dictionary, and cleaning the DataFrame,
+    essentially the main algorithm for the cleaning of the raw data
+
+    Parameters:
+    hard_coded_link (str): The hard-coded link to the data source.
+
+    Returns:
+    pd.DataFrame, pd.DataFrame: The original DataFrame and the cleaned DataFrame.
+    
+    >>> cleaning_process(hard_coded_link)
+    # Original and cleaned DataFrames
+    """
+    url = generate_url(hard_coded_link)
+    df_raw = load_csv(url)
+    projects_df = construct_projects_df(df_raw)
+    correction_dict = generate_correction_dict(projects_df)
+    df = clean_df(df_raw,correction_dict)
+    return df_raw,df
+# """
+# Sample implementation
+# """
+if __name__ == '__main__':
+    cleaning_process('https://drive.google.com/file/d/1UAO5oOF1-BdDUFSySa9roIkOwytqtguO/view')
---
+++ py_functions/gbq_utils.py
@@ -0,0 +1,187 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 26 22:54:12 2023
+
+@author: Arvin Jay
+"""
+import time,json
+from google.cloud import bigquery
+from google.oauth2 import service_account
+import pandas as pd
+
+def authenticate_bq(acct):
+    """
+    Authenticate to Google BigQuery personal trial service account.
+
+    Parameters:
+    acct (dict): secrets.json file from generated key
+
+    Returns:
+    tuple: A tuple containing the BigQuery client and credentials.
+    """
+    credentials = service_account.Credentials.from_service_account_info(
+        acct
+    )
+    client = bigquery.Client(credentials=credentials,project=credentials.project_id)
+    return client,credentials
+
+def check_dataset(client,project_id,dataset_name):
+    """
+    Create a bigquery dataset if it does not exist.
+
+    Parameters:
+    client (bigquery.Client): The BigQuery client.
+    project_id (str): The BigQuery project ID.
+    dataset_name (str): The name of the dataset to check/create.
+
+    Returns:
+    None but status is printed in console 
+    """
+    datasets = [i.dataset_id for i in list(client.list_datasets())]
+    if dataset_name not in datasets:
+        platform_dataset = f"{project_id}.{dataset_name}" #format "project_id.platform" ie "lica-rdbms.rapide"
+        dataset = bigquery.Dataset(platform_dataset)
+        dataset.location = "US"
+        dataset = client.create_dataset(dataset, timeout=30)
+        print("Created dataset {}".format(platform_dataset))
+        datasets = [i.dataset_id for i in list(client.list_datasets())]
+        print("Updated GCP-Bigquery datasets")
+        print(datasets)
+    else:
+        print("{} already in GCP-Bigquery".format(dataset_name.title()))
+        pass
+
+
+def bq_write(df,credentials,dataset_name,table_name,client,date_field='timestamp'):
+    """
+    Write a pandas DataFrame to a BigQuery table and is partitioned based on
+    the date_field column per DAY. This saves us costs when we are dealing
+    with a load of data.
+
+    Parameters:
+    df (pd.DataFrame): The DataFrame to write.
+    credentials (google.auth.credentials.Credentials): credentials object 
+    generated earlier.
+    dataset_name (str): BigQuery dataset name
+    table_name (str): BigQuery table name
+    client (bigquery.Client): BigQuery client.
+    date_field (str): The name of the date field in the DataFrame 
+    to be partitioned.
+
+    Returns:
+    tuple: A tuple containing success (bool) and an error message (str).
+    """
+    success = False
+    error = None
+    if len(df)==0:
+        error = 'No dataframe to be transferred'
+        return success,error
+    
+    job_config = bigquery.LoadJobConfig(
+                write_disposition = 'WRITE_TRUNCATE',
+                source_format=bigquery.SourceFormat.CSV,
+                autodetect=True,
+                time_partitioning=bigquery.TimePartitioning(
+                        type_=bigquery.TimePartitioningType.DAY,
+                        field=date_field
+                    )
+                )
+    target_table_id = f"{credentials.project_id}.{dataset_name}.{table_name}"
+    job = client.load_table_from_dataframe(
+                df, 
+                target_table_id,
+                job_config=job_config)
+    while job.state != "DONE":
+        time.sleep(2)
+        job.reload()
+    print(job.result())
+    table =client.get_table(target_table_id)
+    if table.num_rows==len(df):
+        success = True
+    print(
+          'Loaded {} rows and {} columns to {}'.format(
+              table.num_rows, len(table.schema), target_table_id)
+          )
+    return success,error
+
+
+
+def secrets_configs(secret = 'secrets.json',config = 'config.json'):
+    """
+    Load secrets and configuration which are json files.
+
+    Parameters:
+    secret (str): The path to the secrets JSON file.
+    config (str): The path to the configuration JSON file.
+
+    Returns:
+    tuple: A tuple containing secrets (dict), dataset_name (str), and table_name (str).
+    """
+    secrets = open('secrets.json')
+    secrets = json.load(secrets)
+    config = open('config.json')
+    config = json.load(config)
+    dataset_name = config['tm_dataset']
+    table_name = config['tm_table']
+    return secrets,dataset_name,table_name
+
+def write_process(df):
+    """
+    Main algorithm for writing a table in Google Bigquery. this contains the
+    functions described above
+
+    Parameters:
+    df (pd.DataFrame): The DataFrame to write.
+
+    Returns:
+    tuple: A tuple containing success (bool) and an error message (str).
+    """
+    secrets,dataset_name,table_name = secrets_configs()
+    client,credentials = authenticate_bq(secrets)
+    check_dataset(client,credentials.project_id,dataset_name)
+    success,error = bq_write(df,credentials,dataset_name,table_name,client)
+    return success,error
+
+def generate_table_id(credentials,dataset_name,table_name):
+    """
+    Generate the table ID for a BigQuery table to prepare for querying
+    GBQ table.
+
+    Parameters:
+    credentials (google.auth.credentials.Credentials): Google Cloud credentials.
+    dataset_name (str): The name of the BigQuery dataset.
+    table_name (str): The name of the BigQuery table.
+
+    Returns:
+    str: The full table ID.
+    """
+    return f'{credentials.project_id}.{dataset_name}.{table_name}'
+
+def query_bq_table():
+    """
+    Query an entire table in GBQ. Although it's not standard to query
+    an entire table, I thought it would be better to cache the query result
+    instead of making the query again and again over a small dataset
+
+    Returns:
+    pd.DataFrame: The queried data as a DataFrame.
+    """
+    secrets,dataset_name,table_name = secrets_configs()
+    client,credentials = authenticate_bq(secrets)
+    table_id = generate_table_id(credentials,dataset_name,table_name)
+    query_str = f"""
+                SELECT * FROM `{table_id}`
+    """
+    response = client.query(query_str).to_dataframe()
+    return response
+
+# """
+# Sample implementation
+# """
+if __name__ == '__main__':
+    secrets,dataset_name,table_name = secrets_configs()
+    client,credentials = authenticate_bq(secrets)
+    check_dataset(client,credentials.project_id,dataset_name)
+    df = pd.DataFrame()
+    success,error = bq_write(df,credentials,dataset_name,table_name,client)
+
